571806929330c830b499587629c0085d1a4b5729
Grant Murphy <grantcmurphy@gmail.com>
2012-03-29 09:07:57 +1000

    go version 1 changes

--- server/main.go
+++ server/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "http"
+	"os"
+	"fmt"
+	"net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello World!")
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
 
-func main(){
-    ip := os.Getenv("OPENSHIFT_INTERNAL_IP")
-    http.HandleFunc("/", hello)
-    http.ListenAndServe(fmt.Sprintf("%s:8080", ip), nil)
+func main() {
+	ip := os.Getenv("OPENSHIFT_INTERNAL_IP")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(fmt.Sprintf("%s:8080", ip), nil)
 }
